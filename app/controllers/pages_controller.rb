2496fac3e7b5ebe5c306d9aa75310e05e48806e3
JandersonCRB <janderson@viajamont.com>
2018-04-06 16:54:05 -0300

    last changes before presentation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
 	def index
+		if current_user&.mechanical
+			redirect_to fixes_url
+		end
+	end
+
+	def problems
+	end
+	
+	def help
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+  get '/problems', to: 'pages#problems'
+  get '/help', to: 'pages#help'
 end
